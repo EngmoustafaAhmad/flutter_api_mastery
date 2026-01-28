b502fd27b4668ecacd1d88f7139de08edff75ac1
Moustafa Ahmad <moustafaahmed51015@gmail.com>
2026-01-28 20:45:06 +0200

    Create state.dart

---
+++ bloc (cubit) StateManagement/state.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+class CounterState extends Equatable {
+  final int count;
+
+  const CounterState({required this.count});
+
+  @override
+  List<Object> get props => [count];
+
+  CounterState copyWith({int? count}) {
+    return CounterState(
+      count: count ?? this.count,
+    );
+  }
+}
