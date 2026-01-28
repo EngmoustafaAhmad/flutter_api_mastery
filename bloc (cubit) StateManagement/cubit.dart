95696ffdcb25c9346c07d8de64b8989dec5ad886
Moustafa Ahmad <moustafaahmed51015@gmail.com>
2026-01-28 20:42:07 +0200

    Create cubit.dart

---
+++ bloc (cubit) StateManagement/cubit.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(const CounterState(count: 0));
+
+  void increment() {
+    emit(state.copyWith(count: state.count + 1));
+  }
+
+  void decrement() {
+    emit(state.copyWith(count: state.count - 1));
+  }
+
+  void reset() {
+    emit(const CounterState(count: 0));
+  }
+}
