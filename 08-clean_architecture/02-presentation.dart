3a059ad04e610886fff4481c4b68b6230711a6dc
Moustafa Ahmad <moustafaahmed51015@gmail.com>
2026-02-03 14:55:48 +0200

    Create 02-presentation.dart

---
+++ 08-clean_architecture/02-presentation.dart
@@ -0,0 +1,9 @@
+class UserPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("User Profile")),
+      body: Center(child: Text("User details here")),
+    );
+  }
+}
